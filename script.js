1994be41107bfb6e10bd3c69965d6c7daa6dadde
dkoumond <59847787+dkoumond@users.noreply.github.com>
2020-02-05 15:53:28 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-var wrapperEle = document.body.querySelector(".wrapper");
-var buttonEle = document.body.querySelector(".clicker");
+var animals = ["dog", "fish", "cat", "shark", "cat", "dog"];
+var arrayLength = animals.length;
+var msg = '';
+var index;
 
-function calculate(message1,message2,mpg){
-  var milesPerYear = Number(prompt("What is your estimate number of mile per year?"));
-  var currentGasCost = Number(prompt("What is the current cost of gasoline in your area?"));
-  var totalCost = (milesPerYear / mpg)*currentGasCost;
-  wrapperEle.innerHTML = message1+mpg+" for "+milesPerYear+" miles at $"+currentGasCost+message2+totalCost;
+for(index=0; index<arrayLength; index++){
+  if(animals[index]=='cat'){
+    msg +='I am a cat'+'<br />';
+  } else if (animals[index]=='dog'){
+    msg +='borf borf'+'<br />';
+  } else {
+    msg +='I am an animal'+'<br />';
+  }
 }
-buttonEle.addEventListener("click", function(){
-  calculate("To drive a car with an MPG rating of "," per gallon would cost $",12);
-  calculate("To drive a car with an MPG rating of "," per gallon would cost $",17);
-  calculate("To drive a car with an MPG rating of "," per gallon would cost $",26);
-  calculate("To drive a car with an MPG rating of "," per gallon would cost $",29);
-})
+
+document.getElementById('message').innerHTML = msg;
